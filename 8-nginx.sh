c8fe4fa1841545dbbb9082bf1b56401d527308c1
Prashanth12P <pprashanth2812@gmail.com>
2026-06-24 20:06:58 +0100

    updated sctipt to install nginx

--- 8-nginx.sh
+++ 8-nginx.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-id=$(id -i)
-echo "user id is $id"
+id=$(id -u)
+if [ $id -ne 0]
+then
+echo "user is not super user"
+return 1
+fi
+dnf install nginx -y
+if [ $? -eq 0]
+then
+echo "enginix having some error"
+else
+echo "nginx got installed
+fi
