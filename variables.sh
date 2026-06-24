f758b01508b0d2aaeb05817b8b6046bbd3f10c9e
Prashanth12P <pprashanth2812@gmail.com>
2026-06-24 18:52:49 +0100

    update log for end time

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@ scriptlog=$(date +%s)
 echo "log generated at $scriptlog"
 sleep 10
 endtime=$(date +%s)
-TOTALTIME=($endtime-$scriptlog)
+TOTALTIME=$(($endtime-$scriptlog))
 echo "log generated at end date $endtime"
 echo "testp total time $TOTALTIME"
 
