c701535f2b2e8afa792907daf9d96b5ba5b9e0c9
Prashanth12P <pprashanth2812@gmail.com>
2026-06-24 19:33:59 +0100

    arthamatic variable script

---
+++ arthamaticoperatios.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+variable1=20
+variable2=30
+sum=$(($variable1+$variable2))
+echo "output is $sum"
+fruits=("Apple" "bananna" "mango")
+echo "fruts are ${frutis[@]}"
+echo "fruits are ${fruits[0]}"
+echo "fruit2 is ${fruits[1]}" 
+echo "fruit3 is ${fruits[2]}"
