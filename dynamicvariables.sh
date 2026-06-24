308f53c7b80d7b3c26d2a4f4723ca890af6f4b3f
Prashanth12P <pprashanth2812@gmail.com>
2026-06-24 18:19:32 +0100

    update direct variable to script

---
+++ dynamicvariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1:hello $person2 how are u";
+
+echo "$person2:hello $person1 i am fine thank u how are u";
+
+echo "$person1:hi what are u doing";
+
