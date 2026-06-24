a8a490273cc2e64df18bdc3b194a51c9288323ba
Prashanth12P <pprashanth2812@gmail.com>
2026-06-24 19:51:46 +0100

    update contions script

--- conditions.sh
+++ conditions.sh
@@ -3,6 +3,9 @@ number=$1
 if [ $number -gt 20 ]
 then
     echo "given number $number is greater than 20"
+elif [ $number -eq 20 ]
+then
+echo "given number $number is equal to 20"    
 else
    echo "given number $number is less than 20"    
 fi
