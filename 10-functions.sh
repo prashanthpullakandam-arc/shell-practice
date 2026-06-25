eca32cd5543cc503cbaaceb15ee7317c12c31c1f
Prashanth12P <pprashanth2812@gmail.com>
2026-06-25 16:56:16 +0100

    Function data

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-LOG='/var/logs/shellscript'
-LOGFILE='/var/logs/shellscript/$0.log'
+LOG="/var/logs/shellscript"
+LOGFILE="/var/logs/shellscript/$0.log"
 
 id=$(id -u)
 if [ $id -ne 0 ]
 then
-echo "user is not super user"
+echo "user is not super user" |tee -a $LGOFILE
 exit 1
 fi
 mkdir -p LOG
 validate(){
 if [ $1 -eq 0 ]
 then 
-echo "$2 installed"
+echo "$2 installed" | tee -a $LOGFILE
 else
-echo "$2 got failed"
+echo "$2 got failed" | tee -a $LOGFILE
 fi
 }
 dnf install nginx -y &>>$LOGFILE
