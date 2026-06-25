ce3df9977ae9faa17116cef53936fa4552e1cb1d
Prashanth12P <pprashanth2812@gmail.com>
2026-06-25 16:50:28 +0100

    Function data

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
+LOG='/var/logs/shellscript'
+LOGFILE='/var/logs/shellscript/$0.log'
+
 id=$(id -u)
 if [ $id -ne 0 ]
 then
 echo "user is not super user"
 exit 1
 fi
-
+mkdir -p LOG
 validate(){
-if [ $1 -ne 0 ]
+if [ $1 -eq 0 ]
 then 
 echo "$2 installed"
 else
 echo "$2 got failed"
 fi
 }
-dnf install nginx -y
+dnf install nginx -y &>>$LOGFILE
 echo "value $?"
 validate $? "install nginx"
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 validate $? "install mysql"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOGFILE
 validate $? "install nodejs"
 
 
