e1651bac3082a1a44e66bdda190fae0f958e17ff
Prashanth12P <pprashanth2812@gmail.com>
2026-06-25 17:10:26 +0100

    Function data

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
+LOG="/var/logs/shellscript"
+LOGFILE="/var/logs/shellscript/$0.log"
 
-for i in {1..100}
+id=$(id -u)
+if [ $id -ne 0 ]
+then
+echo "user is not super user" |tee -a $LGOFILE
+exit 1
+fi
+mkdir -p LOG
+validate(){
+if [ $1 -eq 0 ]
+then 
+echo "$2 installed" | tee -a $LOGFILE
+else
+echo "$2 got failed" | tee -a $LOGFILE
+fi
+}
+
+for package in {$@}
 do
-echo "$i"
+dnf list installed $package
+if [$? -ne 0]
+then
+dnf install $package -y &>>$LOGFILE
+validate $? "install $package"
+else
+echo "$package already installed";
 done
+
+
+
